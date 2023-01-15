13128474437bc7dc0d88afd0e624161e44d76900
Ayan Khairollayev <a.khairollayev@astanait.edu.kz>
2023-01-16 00:28:01 +0600

    removed imports

--- src/main/java/kz/khairollayev/springsecuritymaster/config/WebSecurityConfig.java
+++ src/main/java/kz/khairollayev/springsecuritymaster/config/WebSecurityConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -16,7 +12,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests((requests) -> requests.requestMatchers("/success").hasRole("USER_ROLE"))
+                .authorizeHttpRequests((requests) ->
+                        requests.requestMatchers("/success").hasRole("USER_ROLE")
+                )
                 .formLogin().defaultSuccessUrl("/success", true)
                 .and()
                 .httpBasic()
